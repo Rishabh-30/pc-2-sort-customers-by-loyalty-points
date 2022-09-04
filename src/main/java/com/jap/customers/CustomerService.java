95c6958d7a62c83525f748711d7afd6e02aadfe0
Rishabh Pathak <rishabh.pathak30@hotmail.com>
2022-09-04 11:25:43 +0530

    completed with test run

--- src/main/java/com/jap/customers/CustomerService.java
+++ src/main/java/com/jap/customers/CustomerService.java
@@ -6,8 +6,26 @@ import java.util.*;
 public class CustomerService {
     // This method returns the customer's loyalty points in sorted order
 public List<Integer> getListOfCustomersSortedByLoyaltyPoints(List<Customer> customerList, LoyaltyPointComparator loyaltyPointComparator){
-
-    return null;
+    List<Integer> integerList = new ArrayList<>();
+    Collections.sort(customerList,loyaltyPointComparator);
+    Iterator<Customer> iterator = customerList.iterator();
+    while(iterator.hasNext()){
+        integerList.add(iterator.next().getLoyaltyPoints());
+    }
+    return integerList;
 }
 
+    public static void main(String[] args) {
+        CustomerService customerService = new CustomerService();
+        List<Customer> customerList = new ArrayList<>();
+        customerList.add(new Customer(10,"Abhishek","Male",false,"Indore",240));
+        customerList.add(new Customer(11,"Vaibhav","Male",false,"Indore",180));
+        customerList.add(new Customer(12,"Diwakar","Male",false,"Indore",200));
+
+
+        System.out.println(customerService.getListOfCustomersSortedByLoyaltyPoints(customerList, new LoyaltyPointComparator()));
+    }
+
 }
+
+
